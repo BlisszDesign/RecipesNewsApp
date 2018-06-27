64a0c239258cc5e432672e3fe985309cb09d9e9a
Eszti <estera.tothova@gmail.com>
2018-06-27 15:47:24 +0200

    News app stage 2 - adding sharedpreferences

--- app/src/main/java/com/example/android/recipes/RecipesActivity.java
+++ app/src/main/java/com/example/android/recipes/RecipesActivity.java
@@ -4,11 +4,15 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -21,7 +25,7 @@ import java.util.List;
 public class RecipesActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Recipes>> {
 
     private static final String GUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?from-date=2017-01-01&show-fields=byline%2Cthumbnail&page-size=15&q=recipes&api-key=";
+            "http://content.guardianapis.com/search?show-fields=byline%2Cthumbnail";
 
     private static final int RECIPES_LOADER_ID = 1;
 
@@ -67,7 +71,24 @@ public class RecipesActivity extends AppCompatActivity implements LoaderManager.
 
     @Override
     public Loader<List<Recipes>> onCreateLoader(int id, Bundle bundle) {
-        return new RecipesLoader(this, GUARDIAN_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String maxRecipes = sharedPrefs.getString(getString(R.string.settings_max_recipes_key),
+                getString(R.string.settings_max_recipes_default));
+
+        String orderBy = sharedPrefs.getString(getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("from-date", "2017-01-01");
+        uriBuilder.appendQueryParameter("q", "recipes");
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("page-size", maxRecipes);
+        uriBuilder.appendQueryParameter("api-key", "0869c196-c93c-4456-b45c-d6df75e35f16");
+
+        return new RecipesLoader(this, uriBuilder.toString());
+
     }
 
     @Override
@@ -88,5 +109,22 @@ public class RecipesActivity extends AppCompatActivity implements LoaderManager.
     @Override
     public void onLoaderReset(Loader<List<Recipes>> loader) {
         loader.reset();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
